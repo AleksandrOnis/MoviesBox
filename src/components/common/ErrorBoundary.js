dfab60c75d226d61fd27db3fe41c2090ff2ba5ba
AleksandrOnis <sashaonis@gmail.com>
2022-04-29 10:08:19 +0300

    Create ErrorBoundary.js

---
+++ src/components/common/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('ErrorBoundary - error:', error);
+    console.log('ErrorBoundary - errorInfo:', errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oops, something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
